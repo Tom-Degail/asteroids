347c4d202c34bf10dfc9adb7cf4aed3bdfdb93f0
tdegail <tdeg@tutanota.com>
2025-11-20 16:42:50 +0100

    refresh rate

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from logger import log_state
 
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver }")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while 1:
         log_state()
         for event in pygame.event.get():
@@ -17,5 +19,6 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 if __name__ == "__main__":
     main()
